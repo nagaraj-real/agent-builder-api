6a15a7527ee98d6a0609473f639aafc7da66b688
nagaraj <raj.nagaraj1990@gmail.com>
2024-05-29 13:46:09 -0400

    fix: update default cohere model

--- agentbuilder/llm/cohere_llm.py
+++ agentbuilder/llm/cohere_llm.py
@@ -5,7 +5,7 @@ def cohere_chat(*args,**kwargs):
     from langchain_cohere import ChatCohere
     api_key = os.environ['COHERE_API_KEY']
     model= kwargs.get("model")
-    model = "command" if model is None else model
+    model = "command-r" if model is None else model
     logger.info(f"using model {model}")
     kwargs.update({"cohere_api_key":api_key,"model":model})
     llm= ChatCohere(*args,**kwargs)
